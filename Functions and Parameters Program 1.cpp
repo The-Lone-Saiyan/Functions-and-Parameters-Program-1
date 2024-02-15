df7c8cfdf81dc8c16c298d66f0f24ddc18bd06f4
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-15 18:51:54 -0500

    In Progress

--- Functions and Parameters Program 1.cpp
+++ Functions and Parameters Program 1.cpp
@@ -6,10 +6,21 @@ using namespace std;
 
 double X1, X2, Y1, Y2;
 
+double DistanceFormula(double X1, double X2, double Y1, double Y2)
+{
+	double Distance;
+
+	Distance = sqrt(pow((X2 - X1), 2) + pow((Y2 - Y1), 2));
+
+	return Distance;
+}
+
 
 int main()
 {
  
+	DistanceFormula(X1, X2, Y1, Y2);
+
 	cout << "Welcome to the Distance, Radius, Circumference, and Area calculator.\n" << endl;
 	cout << "Please enter the value for X1: ";
 		cin >> X1;
@@ -22,6 +33,8 @@ int main()
 
 	cout << "\nPlease enter the value for Y2: ";
 		cin >> Y2;
+
+		cout << "\nDistance: " << DistanceFormula(X1, X2, Y1, Y2) << endl;
 
 	return 0;
 }
