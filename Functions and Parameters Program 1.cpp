f857078eb18c30c063b3058c9e7a5bc405683313
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-15 18:10:47 -0500

    Add project files.

---
+++ Functions and Parameters Program 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double X1, X2, Y1, Y2;
+
+
+int main()
+{
+ 
+	cout << "Welcome to the Distance, Radius, Circumference, and Area calculator.\n" << endl;
+	cout << "Please enter the value for X1: ";
+		cin >> X1;
+
+	cout << "\nPlease enter the value for X2: ";
+		cin >> X2;
+
+	cout << "\nPlease enter the value for Y1: ";
+		cin >> Y1;
+
+	cout << "\nPlease enter the value for Y2: ";
+		cin >> Y2;
+
+	return 0;
+}
