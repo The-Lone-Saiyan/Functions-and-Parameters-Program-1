16d2939dd3e326231c8de9b16d3326a942ed3d3a
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-15 19:50:38 -0500

    In Progress

--- Functions and Parameters Program 1.cpp
+++ Functions and Parameters Program 1.cpp
@@ -4,15 +4,39 @@
 
 using namespace std;
 
-double X1, X2, Y1, Y2;
+double X1, X2, Y1, Y2, Pi;
+
 
 double DistanceFormula(double X1, double X2, double Y1, double Y2)
 {
-	double Distance;
-
-	Distance = sqrt(pow((X2 - X1), 2) + pow((Y2 - Y1), 2));
+	double Distance = sqrt(pow((X2 - X1), 2) + pow((Y2 - Y1), 2));
 
 	return Distance;
+}
+
+double RadiusFormula(double X2, double Y2)
+{
+	double Radius = sqrt(pow((X2), 2) + pow((Y2), 2));
+
+	return Radius;
+}
+
+double CircumferenceFormula(double Pi)
+{
+	Pi = 3.1416;
+
+	double Circumference = 2 * Pi * RadiusFormula(X2, Y2);
+
+	return Circumference;
+}
+
+double AreaFormula(double Pi)
+{
+	Pi = 3.1416;
+
+	double Area = 2 * Pi * pow(RadiusFormula(X2, Y2), 2);
+
+	return Area;
 }
 
 
@@ -35,6 +59,12 @@ int main()
 		cin >> Y2;
 
 		cout << "\nDistance: " << DistanceFormula(X1, X2, Y1, Y2) << endl;
+
+		cout << "\nRadius: " << RadiusFormula(X2, Y2) << endl;
+
+		cout << "\nCircumference: " << CircumferenceFormula(Pi) << endl;
+
+		cout << "\nArea: " << AreaFormula(Pi) << endl;
 
 	return 0;
 }
