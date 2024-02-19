4c8f669186a562dd0312da5d5dd594c8bb3b5943
Roberto Fitzhugh <rgf274@email.vccs.edu>
2024-02-19 14:15:34 -0500

    Final

--- Functions and Parameters Program 1.cpp
+++ Functions and Parameters Program 1.cpp
@@ -2,39 +2,43 @@
 #include <cmath>
 #include <iomanip>
 
+//Calculator for finding Distance, Radius, Circumference, and Area
+//User only needs to input coordinates for X1, X2, Y1, and Y2
+//Output will be all four answers using the formulas behind the scenes
+
 using namespace std;
 
 double X1, X2, Y1, Y2, Pi;
 
 
-double DistanceFormula(double X1, double X2, double Y1, double Y2)
+double calcDistance(double X1, double X2, double Y1, double Y2)
 {
 	double Distance = sqrt(pow((X2 - X1), 2) + pow((Y2 - Y1), 2));
 
 	return Distance;
 }
 
-double RadiusFormula(double X2, double Y2)
+double calcRadius(double X2, double Y2)
 {
 	double Radius = sqrt(pow((X2), 2) + pow((Y2), 2));
 
 	return Radius;
 }
 
-double CircumferenceFormula(double Pi)
+double calcCircumference(double Pi)
 {
 	Pi = 3.1416;
 
-	double Circumference = 2 * Pi * RadiusFormula(X2, Y2);
+	double Circumference = 2 * Pi * calcRadius(X2, Y2);
 
 	return Circumference;
 }
 
-double AreaFormula(double Pi)
+double calcArea(double Pi)
 {
 	Pi = 3.1416;
 
-	double Area = 2 * Pi * pow(RadiusFormula(X2, Y2), 2);
+	double Area = 2 * Pi * pow(calcRadius(X2, Y2), 2);
 
 	return Area;
 }
@@ -43,7 +47,7 @@ double AreaFormula(double Pi)
 int main()
 {
  
-	DistanceFormula(X1, X2, Y1, Y2);
+	calcDistance(X1, X2, Y1, Y2);
 
 	cout << "Welcome to the Distance, Radius, Circumference, and Area calculator.\n" << endl;
 	cout << "Please enter the value for X1: ";
@@ -58,13 +62,13 @@ int main()
 	cout << "\nPlease enter the value for Y2: ";
 		cin >> Y2;
 
-		cout << "\nDistance: " << DistanceFormula(X1, X2, Y1, Y2) << endl;
+		cout << "\nDistance: " << calcDistance(X1, X2, Y1, Y2) << endl;
 
-		cout << "\nRadius: " << RadiusFormula(X2, Y2) << endl;
+		cout << "\nRadius: " << calcRadius(X2, Y2) << endl;
 
-		cout << "\nCircumference: " << CircumferenceFormula(Pi) << endl;
+		cout << "\nCircumference: " << calcCircumference(Pi) << endl;
 
-		cout << "\nArea: " << AreaFormula(Pi) << endl;
+		cout << "\nArea: " << calcArea(Pi) << endl;
 
 	return 0;
 }
