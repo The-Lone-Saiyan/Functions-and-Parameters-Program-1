c5469a816f0eb0dfb9d813f331b2337fb50d0b96
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-24 16:25:32 -0500

    In Progress

--- Functions and Parameters Program 1.cpp
+++ Functions and Parameters Program 1.cpp
@@ -7,8 +7,42 @@
 
 using namespace std;
 
-double X1, X2, Y1, Y2, Pi;
+int main()
+{
+	double X1, X2, Y1, Y2, Pi;
 
+	double calcDistance(double X1, double X2, double Y1, double Y2);
+
+	double calcRadius(double X2, double Y2);
+
+	double calcCircumference(double Pi);
+
+	double calcArea(double Pi);
+
+
+	cout << "Welcome to the Distance, Radius, Circumference, and Area calculator.\n" << endl;
+	cout << "Please enter the value for X1: ";
+		cin >> X1;
+
+	cout << "\nPlease enter the value for X2: ";
+		cin >> X2;
+
+	cout << "\nPlease enter the value for Y1: ";
+		cin >> Y1;
+
+	cout << "\nPlease enter the value for Y2: ";
+		cin >> Y2;
+
+		cout << "\nDistance: " << calcDistance(X1, X2, Y1, Y2) << endl;
+
+		cout << "\nRadius: " << calcRadius(X2, Y2) << endl;
+
+		cout << "\nCircumference: " << calcCircumference(Pi) << endl;
+
+		cout << "\nArea: " << calcArea(Pi) << endl;
+
+	return 0;
+}
 
 double calcDistance(double X1, double X2, double Y1, double Y2)
 {
@@ -40,34 +74,4 @@ double calcArea(double Pi)
 	double Area = Pi * pow(calcRadius(X2, Y2), 2);
 
 	return Area;
-}
-
-
-int main()
-{
- 
-	calcDistance(X1, X2, Y1, Y2);
-
-	cout << "Welcome to the Distance, Radius, Circumference, and Area calculator.\n" << endl;
-	cout << "Please enter the value for X1: ";
-		cin >> X1;
-
-	cout << "\nPlease enter the value for X2: ";
-		cin >> X2;
-
-	cout << "\nPlease enter the value for Y1: ";
-		cin >> Y1;
-
-	cout << "\nPlease enter the value for Y2: ";
-		cin >> Y2;
-
-		cout << "\nDistance: " << calcDistance(X1, X2, Y1, Y2) << endl;
-
-		cout << "\nRadius: " << calcRadius(X2, Y2) << endl;
-
-		cout << "\nCircumference: " << calcCircumference(Pi) << endl;
-
-		cout << "\nArea: " << calcArea(Pi) << endl;
-
-	return 0;
 }
